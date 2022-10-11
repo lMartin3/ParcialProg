38650bc223af5b4135f703120c324b13f8aa29c8
lMartin3 <martinlufr@gmail.com>
2022-10-11 11:51:27 -0300

    Agregar archivos de proyecto.

---
+++ ParcialMartinLuraschiEj2/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ParcialMartinLuraschiEj2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbResultado = new System.Windows.Forms.TextBox();
+            this.btnPresupuestar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.tbNombreCliente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.presupuesto = new System.Windows.Forms.DataGridView();
+            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.presupuesto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbResultado
+            // 
+            this.tbResultado.Location = new System.Drawing.Point(12, 305);
+            this.tbResultado.Multiline = true;
+            this.tbResultado.Name = "tbResultado";
+            this.tbResultado.Size = new System.Drawing.Size(866, 133);
+            this.tbResultado.TabIndex = 0;
+            this.tbResultado.TextChanged += new System.EventHandler(this.tbResultado_TextChanged);
+            // 
+            // btnPresupuestar
+            // 
+            this.btnPresupuestar.Location = new System.Drawing.Point(769, 135);
+            this.btnPresupuestar.Name = "btnPresupuestar";
+            this.btnPresupuestar.Size = new System.Drawing.Size(109, 39);
+            this.btnPresupuestar.TabIndex = 1;
+            this.btnPresupuestar.Text = "Presupuestar";
+            this.btnPresupuestar.UseVisualStyleBackColor = true;
+            this.btnPresupuestar.Click += new System.EventHandler(this.btnPresupuestar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(769, 180);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(109, 39);
+            this.btnLimpiar.TabIndex = 2;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(769, 225);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(109, 39);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // tbNombreCliente
+            // 
+            this.tbNombreCliente.Location = new System.Drawing.Point(37, 44);
+            this.tbNombreCliente.Name = "tbNombreCliente";
+            this.tbNombreCliente.Size = new System.Drawing.Size(215, 22);
+            this.tbNombreCliente.TabIndex = 5;
+            this.tbNombreCliente.Tag = "";
+            this.tbNombreCliente.TextChanged += new System.EventHandler(this.tbNombreCliente_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre y apellido del cliente";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // presupuesto
+            // 
+            this.presupuesto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.presupuesto.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.descripcion,
+            this.cantidad,
+            this.precio});
+            this.presupuesto.Location = new System.Drawing.Point(37, 114);
+            this.presupuesto.Name = "presupuesto";
+            this.presupuesto.RowHeadersWidth = 51;
+            this.presupuesto.RowTemplate.Height = 24;
+            this.presupuesto.Size = new System.Drawing.Size(694, 150);
+            this.presupuesto.TabIndex = 7;
+            this.presupuesto.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // descripcion
+            // 
+            this.descripcion.HeaderText = "Descripción";
+            this.descripcion.MinimumWidth = 6;
+            this.descripcion.Name = "descripcion";
+            this.descripcion.Width = 250;
+            // 
+            // cantidad
+            // 
+            this.cantidad.HeaderText = "Cantidad";
+            this.cantidad.MinimumWidth = 6;
+            this.cantidad.Name = "cantidad";
+            // 
+            // precio
+            // 
+            this.precio.HeaderText = "Precio Unitario";
+            this.precio.MinimumWidth = 6;
+            this.precio.Name = "precio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(570, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Utilize la tabla para agregar productos al presupuesto y luego presione el botón " +
+    "\'Presupuestar\'";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(782, 452);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Martín Luraschi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 477);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.presupuesto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNombreCliente);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnPresupuestar);
+            this.Controls.Add(this.tbResultado);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 2 Parcial I Programación - Martín Luraschi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.presupuesto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbResultado;
+        private System.Windows.Forms.Button btnPresupuestar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox tbNombreCliente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView presupuesto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ParcialMartinLuraschiEj2/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParcialMartinLuraschiEj2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MostrarErrorCliente()
+        {
+            MessageBox.Show("Usted debe ingresar el nombre del cliente!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void MostrarErrorValor(int filaIndex, string campo)
+        {
+            MessageBox.Show(string.Format("El campo \"{0}\" en la columna {1} contiene un valor no numérico", campo, filaIndex + 1), "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+
+        private void btnPresupuestar_Click(object sender, EventArgs e)
+        {
+            if (tbNombreCliente.Text == "")
+            {
+                MostrarErrorCliente();
+                return;
+            }
+
+            string resultado = "Presupuesto para el cliente " + tbNombreCliente.Text + "\r\n"; //TODO nombre cliente
+            int precioTotal = 0;
+            for (int i = 0; i < presupuesto.Rows.Count; i++)
+            {
+                if (presupuesto.Rows[i].Cells[0].Value == null) continue;
+                string producto = presupuesto.Rows[i].Cells[0].Value.ToString();
+                int cantidad = 0;
+                bool parsedFirst = int.TryParse(presupuesto.Rows[i].Cells[1].Value.ToString(), out cantidad);
+                if (!parsedFirst)
+                {
+                    MostrarErrorValor(i, "cantidad");
+                }
+                int precioUnitario = 0;
+                bool parsedSecond = int.TryParse(presupuesto.Rows[i].Cells[2].Value.ToString(), out precioUnitario);
+                if (!parsedSecond)
+                {
+                    MostrarErrorValor(i, "precio unitario");
+                }
+                int precioPruducto = precioUnitario * cantidad;
+                precioTotal += precioPruducto;
+                resultado += String.Format("{0}x {1} a ${2} p/u (subtotal ${3})\r\n", cantidad, producto, precioUnitario, precioPruducto);
+            }
+            resultado += string.Format("\r\n\r\nTOTAL DEL PRESUPUESTO: ${0}", precioTotal);
+            tbResultado.Text = resultado;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            tbNombreCliente.Text = "";
+            tbResultado.Text = "";
+            while(presupuesto.Rows.Count>=2)
+            {
+                presupuesto.Rows.RemoveAt(0);
+            }
+
+        
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).FindForm().Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void tbResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNombreCliente_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
